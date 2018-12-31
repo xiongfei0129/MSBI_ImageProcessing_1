547ca3277424a8eb19b330155b0456b99d64dba1
xiongfei <xiongfei0129@gmail.com>
2018-12-30 20:12:32 -0800

    commit 2

--- Reduced_KspaceSampling.m
+++ Reduced_KspaceSampling.m
@@ -1,7 +1,6 @@
 %% Reduced K-space Processing
+%% Randomly sample k-space data and recontruct image after sampling.
 clear all;close all
-
-% Randomly sample k-space data and recontruct image after sampling.
 I=imread('AxT1_brain.jpg');
 I=rgb2gray(I);
 I_fft=fft2(I);
@@ -24,3 +23,49 @@ I_noise=ifft2(fftshift(I_fft_sampled));% ifft2 has inbuilt shift function???
 
 subplot(2,2,4)
 imagesc(abs(I_noise));colormap gray;
+
+%% Selectively sample k-space data and reconstruct image after sampling. 
+clear all;close all
+I=imread('AxT1_brain.jpg');
+I=rgb2gray(I);
+I_fft=fft2(I);
+I_fftS=fftshift(I_fft);
+
+rows=size(I_fftS,1);
+columns=size(I_fftS,2);
+I_fftS_new=zeros(size(I_fftS));
+
+mask_matrix=zeros(1,columns);
+
+% Alternating
+for i=1:rows
+    if rem(i,2)==0
+        I_fftS_newA(i,:)=I_fftS(i,:).*mask_matrix;
+    else
+        I_fftS_newA(i,:)=I_fftS(i,:);
+    end
+end
+
+% Upper&Lower
+for i=1:rows
+    if i<220||i>660
+        I_fftS_newB(i,:)=I_fftS(i,:).*mask_matrix;
+    else
+        I_fftS_newB(i,:)=I_fftS(i,:);
+    end
+end
+
+I_newA=ifft2(I_fftS_newA);
+I_newB=ifft2(I_fftS_newB);
+
+subplot(2,2,1)
+imagesc(abs(I_fftS_newA), [0 10000]);colormap gray;
+
+subplot(2,2,2)
+imagesc(abs(I_fftS_newB), [0 10000]);colormap gray;
+
+subplot(2,2,3)
+imagesc(abs(I_newA), [0 1000]);colormap gray;
+
+subplot(2,2,4)
+imagesc(abs(I_newB), [0 1000]);colormap gray;
